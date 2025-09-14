a4e3bd861f23d7aa71944bf2d6951b8a1d026003
jamestangerine <haydenhellsten@icloud.com>
2025-09-14 11:32:55 -0400

    Initial commit

---
+++ prog.c
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+uint8_t add(uint8_t a, uint8_t b) {
+    return a + b;
+}
---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_LIST = [
+    "/",
+    "/index.html",
+    "/prog.js",
+    "/prog.wasm",
+    "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("v1").then((cache) => {
+            return cache.addAll(CACHE_LIST);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== "v1") {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
